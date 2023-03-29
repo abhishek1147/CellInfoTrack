26f38a8c3e16eb9054e1868aa0e445e09ad1dd9a
abhiTibrewal <96610020+abhiTibrewal@users.noreply.github.com>
2023-03-29 13:45:27 +0530

    Logs added

--- app/src/main/java/com/zonedevs/cellinfotracker/MainActivity.kt
+++ app/src/main/java/com/zonedevs/cellinfotracker/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.zonedevs.cellinfotracker
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainerView
@@ -22,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("onCreate","Project Created")
         setupView()
     }
 
